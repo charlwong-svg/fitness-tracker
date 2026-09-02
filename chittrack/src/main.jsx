c21fb73de4b139e0146b2755962a60eb5748eab9
charlwong-svg <charlwong@gmail.com>
2026-09-02 14:41:47 +0800

    Add files via upload

--- chittrack/src/main.jsx
+++ chittrack/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChitTrack from "./App.jsx";
+import MyFitnessTracker from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChitTrack />
+    <MyFitnessTracker />
   </React.StrictMode>
 );
